afbbc785f4fc062bc2206e74337bc143ddcd237f
Nick Bevers <nick.bevers9@gmail.com>
2024-05-20 13:48:25 +0200

    feat: cleanup unneeded files and styling

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Nick Bevers';
+export const SITE_TITLE = 'Nick\'s Blog';
+export const SITE_NAME = 'Nick Bevers';
+export const SITE_URL = 'https://blog.nickbevers.be';
 export const SITE_DESCRIPTION = 'Blogging since 2024';
